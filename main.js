3ac2af54cbea9502494a7b133f13e06eca21cdda
ThetSuMonZaww <thetsumonzaw92@gmail.com>
2022-05-17 23:50:10 +0630

    commited

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.scss'
+
+import 'bootstrap';
+import 'waypoints/lib/noframework.waypoints.min';
+import ScrollReveal from "scrollreveal";
+import Typed from "typed.js";
+
+let rating_box = document.querySelectorAll('.rating-box');
+rating_box.classList.add('animate__animated','animate__shakeY');
